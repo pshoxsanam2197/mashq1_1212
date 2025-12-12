9a1d52c43d9880710176542abaccfd7c11159f97
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2025-12-12 16:09:54 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+print(144**0.5)
+
+print(225**0.5)
+
+import math
+print(math.sqrt(169))
+
+import math
+print(math.sqrt(81))
+
+import math
+print(math.sqrt(400))
+
+import math
+print(math.sqrt(121))
+
+import math
+print(math.sqrt(49))
+
+import math
+print(math.sqrt(625))
